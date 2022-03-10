95da10cf50541ba706b344b0274a5a8f6668eef7
Zunaid Ali <zpokean@gmail.com>
2022-03-10 11:50:34 -0800

    Fix quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - main function of the program that will print the desired string
- * Return: Will return 1
+ * main - prints desired string
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	Return (1);
+	return (1);
 }
