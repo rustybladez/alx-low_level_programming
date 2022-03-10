98f25335024f0637ba84ba647ae77ad4efcd981b
Zunaid Ali <zpokean@gmail.com>
2022-03-10 08:41:02 -0800

    Add printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main function of the program
+ * Return: 0 to terminate the program
+ */
+int main(void)
+{
+ 	printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
