f34cc3d776ee7608da50bb19c1ec2852121a3230
Zunaid Ali <zpokean@gmail.com>
2022-03-10 09:40:47 -0800

    Add size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function of the program
+ * Return: 0 to terminate the program
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	return (0);
+}
