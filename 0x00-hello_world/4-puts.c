7c91e317552a04b455c2c3fea5a36a356475d066
Zunaid Ali <zpokean@gmail.com>
2022-03-10 08:30:10 -0800

    Fix puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - main function of the program
+ * Return: 0 to terminate the program
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
