1230d9e105db5aa113cc71c56b326efea796becf
rustybladez <96477152+rustybladez@users.noreply.github.com>
2022-03-11 22:27:47 +0600

    fix betty coding style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  * main - main function of the program
- * @ch: character to be printed
  * Return: Terminates the program
  */
 int main(void)
 {
 	char ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
