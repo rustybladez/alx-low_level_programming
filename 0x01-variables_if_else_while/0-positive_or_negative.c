af62c2b3ce8876d09720fb1aec192074d2eb6e68
rustybladez <96477152+rustybladez@users.noreply.github.com>
2022-03-11 22:21:48 +0600

    Fix betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
+/**
   * main - main function of the program
-  * @n: number to be tested
   * Return: Terminates the program
   */
 int main(void)
@@ -13,14 +12,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} else if(n == 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	} else
-	{	
+	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
