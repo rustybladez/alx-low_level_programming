6080ec08cce4c165bb4c658f34c55766cdc00cce
rustybladez <96477152+rustybladez@users.noreply.github.com>
2022-03-11 23:02:05 +0600

    fix code with condition

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 /**
  * main - main fuction of the program
- * @i: characters to be printed
  * Return: Terminates the program
  */
 int main(void)
 {
 	int i;
-	for(i = 48; i < 57; i++)
+
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
+		if (i == 57)
+			break;
 		putchar(',');
 		putchar(' ');
 	}
-	putchar(57);
+	putchar('\n');
 	return (0);
 }
