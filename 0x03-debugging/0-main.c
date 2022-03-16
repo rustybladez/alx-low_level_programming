5d1abb3612e11e2c18b79e4b743b6ea2b6bbcd83
Zunaid Ali <zpokean@gmail.com>
2022-03-16 05:12:03 -0700

    Add tasks 0-3

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * positive_or_negative - checks if a number is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	positive_or_negative(i);
+	return (0);
+}
+
