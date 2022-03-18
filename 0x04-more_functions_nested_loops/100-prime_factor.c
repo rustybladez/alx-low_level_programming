7b7846b291a4ff4cedc24199616881bdec4fe2d9
rustybladez <96477152+rustybladez@users.noreply.github.com>
2022-03-18 14:55:55 +0600

    fix betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	unsigned long int i, n = 612852475143;
+	unsigned long int i = 3, n = 612852475143;
 
-	for (i = 3; i < 12057; i += 2)
+	for (; i < 12057; i += 2)
 	{
-		while ( n % i == 0 && n != i)
+		while (n % i == 0 && n != i)
 			n /= i;
 	}
 	printf("%lu\n", n);
