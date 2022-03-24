dc9ec2cb6c7a938b47ecbde7066fd5ae28e2cabc
rustybladez <96477152+rustybladez@users.noreply.github.com>
2022-03-24 17:57:32 +0600

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#indef _main_H_
+#ifndef _main_H_
 #define _main_H_
 
 char *_strcat(char *dest, char *src);
@@ -7,6 +7,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
